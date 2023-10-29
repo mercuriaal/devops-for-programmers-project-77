5794859d82cb9ba7592f86711b8d4bc1de688dc2
i.lazarev <i.lazarev@fun-box.ru>
2023-10-29 19:08:33 +0300

    Вынес создание vm в цикл

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -71,11 +71,11 @@ resource "yandex_alb_target_group" "target-group" {
 
   target {
     subnet_id  = yandex_vpc_subnet.subnet-1.id
-    ip_address = yandex_compute_instance.srv1.network_interface[0].ip_address
+    ip_address = yandex_compute_instance.srvs["srv1"].network_interface[0].ip_address
   }
 
   target {
     subnet_id  = yandex_vpc_subnet.subnet-1.id
-    ip_address = yandex_compute_instance.srv2.network_interface[0].ip_address
+    ip_address = yandex_compute_instance.srvs["srv1"].network_interface[0].ip_address
   }
 }
--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,30 +1,9 @@
-resource "yandex_compute_instance" "srv1" {
-  name = "pluto"
-
-  resources {
-    cores  = 2
-    memory = 2
+resource "yandex_compute_instance" "srvs" {
+  for_each = {
+    "srv1" = "pluto",
+    "srv2" = "jupiter"
   }
-
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.img.id
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "${var.app_vm_user}:${var.ssh_key}"
-  }
-}
-
-resource "yandex_compute_instance" "srv2" {
-  name = "jupiter"
-
+  name = each.value
   resources {
     cores  = 2
     memory = 2
